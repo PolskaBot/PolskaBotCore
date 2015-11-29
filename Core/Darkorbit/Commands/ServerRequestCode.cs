58f1d72eebcff8644a692b629b8ed330a051d38d
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2015-11-29 14:36:32 +0100

    added request code response

---
+++ Core/Darkorbit/Commands/ServerRequestCode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MiscUtil.IO;
+
+namespace PolskaBot.Core.Darkorbit.Commands
+{
+    class ServerRequestCode : Command
+    {
+        public const short ID = 14047;
+
+        public int codeLength { get; private set; }
+        public byte[] code { get; private set; }
+        public int fakeSize { get; private set; }
+
+        public ServerRequestCode(EndianBinaryReader reader)
+        {
+            codeLength = reader.ReadInt32();
+            code = reader.ReadBytes(codeLength);
+            fakeSize = reader.ReadInt32();
+        }
+
+        public override short GetID()
+        {
+            return ID;
+        }
+
+        public override void Write()
+        {
+            return;
+        }
+    }
+}
