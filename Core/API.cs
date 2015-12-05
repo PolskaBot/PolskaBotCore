b5e59607b14392719b2671ea441de1610465bf27
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2015-12-05 12:37:52 +0100

    start mapsServer if in PROXY mode

--- Core/API.cs
+++ Core/API.cs
@@ -19,14 +19,24 @@ namespace PolskaBot.Core
 
         MergedClient mergedClient;
 
+        MapsServer mapsServer;
+
         public API(Mode mode = Mode.BOT)
         {
             this.mode = mode;
             mergedClient = new MergedClient(this);
+
+            if(mode == Mode.PROXY)
+            {
+                mapsServer = new MapsServer(9000);
+            }
         }
 
         public void Login(string server, string sid)
         {
+            if(mode == Mode.PROXY) {
+                throw new NotSupportedException("Login cannot be called when in PROXY mode");
+            }
             mergedClient.Connect(server, sid);
             mergedClient.vanillaClient.Send(new ClientVersionCheck(Config.MAJOR, Config.MINOR, Config.BUILD));
         }
