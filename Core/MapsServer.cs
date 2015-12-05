040cd61a58aaa60c4ac81b3ad5c1406a8340a1ac
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2015-12-05 12:32:51 +0100

    added HTTP server

---
+++ Core/MapsServer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bend.Util;
+
+namespace PolskaBot.Core
+{
+    class MapsServer : HttpServer
+    {
+        public MapsServer(int port) : base(port)
+        {
+
+        }
+
+        public override void handleGETRequest(HttpProcessor p)
+        {
+            Console.WriteLine("Received GET request");
+        }
+
+        public override void handlePOSTRequest(HttpProcessor p, StreamReader inputData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
