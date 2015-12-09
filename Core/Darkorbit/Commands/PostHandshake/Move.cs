aadc02bbd7b7ca48a0a15a7e1d0ef56ec5dd18be
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2015-12-09 23:15:48 +0100

    added hero movement command

--- Core/Darkorbit/Commands/PostHandshake/Move.cs
+++ Core/Darkorbit/Commands/PostHandshake/Move.cs
@@ -8,28 +8,31 @@ namespace PolskaBot.Core.Darkorbit.Commands.PostHandshake
 {
     class Move : Command
     {
-        public const ushort ID = 20808;
+        public const ushort ID = 25229;
 
-        public uint x { get; private set; }
-        public uint y { get; private set; }
+        public uint targetX { get; private set; }
+        public uint targetY { get; private set; }
+        public uint currentX { get; private set; }
+        public uint currentY { get; private set; }
 
-        public Move(uint x, uint y)
+        public Move(uint targetX, uint targetY, uint currentX, uint currentY)
         {
-            this.x = x;
-            this.y = y;
+            this.targetX = targetX;
+            this.targetY = targetY;
+            this.currentX = currentX;
+            this.currentY = currentY;
             Write();
         }
 
         public override void Write()
         {
-            packetWriter.Write((short)10);
+            packetWriter.Write(20);
             packetWriter.Write(ID);
-            uint tempX = x >> 2 | x << 30;
-            uint tempY = y << 12 | y >> 20;
-            Console.WriteLine($"Calculated X {tempX} Y {tempY}");
-            packetWriter.Write(tempX);
-            packetWriter.Write(tempY);
-            packetWriter.Write((short)-1675);
+            packetWriter.Write(targetY >> 6 | targetY << 26);
+            packetWriter.Write((short)1668);
+            packetWriter.Write(currentY << 15 | currentY >> 17);
+            packetWriter.Write(currentX >> 7 | this.currentX << 25);
+            packetWriter.Write(targetX >> 9 | targetX << 23);
         }
     }
 }
