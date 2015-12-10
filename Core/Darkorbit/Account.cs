a4810ca2563f45c5c804d7457e07c463260851d8
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2015-12-10 19:19:00 +0100

    created account class

---
+++ Core/Darkorbit/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolskaBot.Core.Darkorbit
+{
+    class Account
+    {
+        // Website credentials
+        public string username { get; private set; }
+        public string password { get; private set; }
+
+        // Server credentials
+        public int userID { get; private set; }
+        public int instanceID { get; private set; }
+        public string sid { get; private set; }
+        public string serverID { get; private set; }
+        public int mapID { get; private set; }
+
+        public Account()
+        {
+
+        }
+
+        public void SetCredentials(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+    }
+}
