75d5203ba51862d23f1dcb3ef951e06ca551bd07
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2015-12-11 19:47:13 +0100

    added simple gui app with minimap

---
+++ PolskaBot/Bot.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace PolskaBot
+{
+    partial class Bot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.minimap = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.minimap)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // minimap
+            // 
+            this.minimap.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(39)))));
+            this.minimap.Location = new System.Drawing.Point(0, 0);
+            this.minimap.Name = "minimap";
+            this.minimap.Size = new System.Drawing.Size(315, 202);
+            this.minimap.TabIndex = 0;
+            this.minimap.TabStop = false;
+            // 
+            // Bot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 201);
+            this.Controls.Add(this.minimap);
+            this.Name = "Bot";
+            this.Text = "PolskaBot";
+            ((System.ComponentModel.ISupportInitialize)(this.minimap)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox minimap;
+    }
+}
+
---
+++ PolskaBot/Bot.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace PolskaBot
+{
+    public partial class Bot : Form
+    {
+
+        Thread renderer;
+
+        int FPS = 60;
+
+        const byte alpha = 216;
+        static Color hero = Color.FromArgb(alpha, 102, 102, 102);
+        static Color box = Color.FromArgb(alpha, 255, 255, 0);
+        static Color boxPirate = Color.Green;
+        static Color boxMemorised = Color.FromArgb(150, 255, 255, 0);
+
+        public Bot()
+        {
+            InitializeComponent();
+            Load += (s, e) => Init();
+            FormClosed += (s, e) => renderer.Abort();
+        }
+
+        private void Init()
+        {
+            renderer = new Thread(new ThreadStart(Render));
+            renderer.Start();
+        }
+
+        private void Render()
+        {
+            while(true)
+            {
+                var bitmap = new Bitmap(minimap.Width, minimap.Height);
+                using (var g = Graphics.FromImage(bitmap))
+                {
+                    DrawPlayer(g, new Point(80, 40));
+                    g.DrawRectangle(new Pen(box), new Rectangle(66, 36, 1, 1));
+                    g.DrawRectangle(new Pen(boxMemorised), new Rectangle(42, 36, 1, 1));
+                    g.DrawRectangle(new Pen(boxPirate), new Rectangle(50, 36, 1, 1));
+                }
+
+                Invoke((MethodInvoker)delegate
+                {
+                    minimap.Image = bitmap;
+                });
+                Task.Delay(1000 / FPS);
+            }
+        }
+
+        private void DrawPlayer(Graphics g, Point point)
+        {
+            g.DrawLine(new Pen(hero), new Point(0, point.X), new Point(minimap.Width, point.X));
+            g.DrawLine(new Pen(hero), new Point(point.Y, 0), new Point(point.Y, minimap.Height));
+        }
+    }
+}
