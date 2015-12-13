948904c4abcaa16f33ad80af8c303e38a574955d
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2015-12-13 23:42:52 +0100

    fixed ship init packet

--- Core/Darkorbit/Commands/PostHandshake/class_326.cs
+++ Core/Darkorbit/Commands/PostHandshake/class_326.cs
@@ -12,7 +12,7 @@ namespace PolskaBot.Core.Darkorbit.Commands.PostHandshake
         public const ushort ID = 18881;
 
         public bool activated { get; private set; }
-        public uint var_2742 { get; private set; }
+        public ushort var_2742 { get; private set; }
         public string var_2555 { get; private set; }
         public int attribute { get; private set; }
         public int name_125 { get; private set; }
@@ -22,13 +22,14 @@ namespace PolskaBot.Core.Darkorbit.Commands.PostHandshake
         {
             this.attribute = reader.ReadInt32();
             this.attribute = this.attribute << 2 | this.attribute >> 30;
-            this.var_2555 = Encoding.UTF8.GetString(reader.ReadBytes(reader.ReadUInt16()));
+            int length = (int)reader.ReadUInt16();
+            this.var_2555 = Encoding.UTF8.GetString(reader.ReadBytes(length));
             this.name_125 = reader.ReadInt32();
             this.name_125 = this.name_125 << 5 | this.name_125 >> 27;
             this.count = reader.ReadInt32();
             this.count = this.count >> 7 | this.count >> 25;
             this.activated = reader.ReadBoolean();
-            this.var_2742 = reader.ReadUInt32();
+            this.var_2742 = reader.ReadUInt16();
         }
 
         public override void Write()
