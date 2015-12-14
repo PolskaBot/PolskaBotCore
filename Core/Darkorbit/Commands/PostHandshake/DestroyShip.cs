24af9cd2b40fbf9bfb93c8daf5aba325580c6575
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2015-12-14 21:54:28 +0100

    remove ship packet. closes #18

---
+++ Core/Darkorbit/Commands/PostHandshake/DestroyShip.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MiscUtil.IO;
+
+namespace PolskaBot.Core.Darkorbit.Commands.PostHandshake
+{
+    class DestroyShip : Command
+    {
+        public const ushort ID = 14332;
+
+        public uint id { get; private set; }
+
+        public DestroyShip(EndianBinaryReader reader)
+        {
+            reader.ReadInt16();
+            id = reader.ReadUInt32();
+            id = id << 9 | id >> 23;
+        }
+
+        public override void Write()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
