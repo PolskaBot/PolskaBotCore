145c797fdb18e276fabb7c96920bbf42753bb13c
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2015-12-14 20:26:09 +0100

    added remove box and ores packet. closes #19 and #20

---
+++ Core/Darkorbit/Commands/PostHandshake/DestroyItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MiscUtil.IO;
+
+namespace PolskaBot.Core.Darkorbit.Commands.PostHandshake
+{
+    class DestroyItem : Command
+    {
+        public const ushort ID = 24098;
+
+        public bool name_44 { get; private set; }
+        public string hash { get; private set; }
+
+        public DestroyItem(EndianBinaryReader reader)
+        {
+            reader.ReadInt16();
+            name_44 = reader.ReadBoolean();
+            hash = Encoding.UTF8.GetString(reader.ReadBytes(reader.ReadUInt16()));
+        }
+
+        public override void Write()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
