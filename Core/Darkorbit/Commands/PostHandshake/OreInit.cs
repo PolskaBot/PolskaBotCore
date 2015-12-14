7398af1bd4d6acaef236ff50fe5d59b0075b0880
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2015-12-14 21:24:42 +0100

    updated ore packet

--- Core/Darkorbit/Commands/PostHandshake/OreInit.cs
+++ Core/Darkorbit/Commands/PostHandshake/OreInit.cs
@@ -9,7 +9,7 @@ namespace PolskaBot.Core.Darkorbit.Commands.PostHandshake
 {
     class OreInit : Command
     {
-        public const ushort ID = 13039;
+        public const ushort ID = 13463;
 
         public string hash { get; private set; }
         public uint x { get; private set; }
@@ -18,13 +18,15 @@ namespace PolskaBot.Core.Darkorbit.Commands.PostHandshake
 
         public OreInit(EndianBinaryReader reader)
         {
-            hash = Encoding.Default.GetString(reader.ReadBytes(reader.ReadUInt16()));
             x = reader.ReadUInt32();
-            x = x >> 12 | x << 20;
+            x = x << 5 | x >> 27;
+            reader.ReadInt16();
+            hash = Encoding.Default.GetString(reader.ReadBytes(reader.ReadUInt16()));
             y = reader.ReadUInt32();
-            y = y << 12 | y >> 20;
+            y = y << 3 | y >> 29;
             reader.ReadUInt16(); // id to reader class, skipped just because
             type = reader.ReadUInt16();
+            reader.ReadUInt16();
 
         }
 
