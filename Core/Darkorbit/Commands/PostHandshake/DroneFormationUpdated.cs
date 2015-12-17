12709dda7c48d6f934da9586fd376f777dcb0b8d
Adam Mańkowski <maniekmanfred69@gmail.com>
2015-12-17 18:47:24 +0100

    Added DroneFormationUpdated packet

---
+++ Core/Darkorbit/Commands/PostHandshake/DroneFormationUpdated.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MiscUtil.IO;
+
+namespace PolskaBot.Core.Darkorbit.Commands.PostHandshake
+{
+    class DroneFormationUpdated : Command
+    {
+        public const ushort ID = 32691;
+
+        public int DroneFormation { get; private set; } //class_411 | var_3642
+        public int UID { get; private set; }
+
+        public DroneFormationUpdated(EndianBinaryReader reader)
+        {
+            reader.ReadUInt16();
+            DroneFormation = reader.ReadInt32();
+            DroneFormation = (int)((uint)DroneFormation >> 12 | (uint)DroneFormation << 20);
+            UID = reader.ReadInt32();
+            UID = (int)((uint)UID >> 13 | (uint)UID << 19);
+        }
+
+        public override void Write()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
