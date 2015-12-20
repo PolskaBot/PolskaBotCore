5d6c3be0e3589fb48ef6db25a81ee22e218b2dcf
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2015-12-20 23:48:18 +0100

    added jump packet

---
+++ Core/Darkorbit/Commands/PostHandshake/Jump.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolskaBot.Core.Darkorbit.Commands.PostHandshake
+{
+    public class Jump : Command
+    {
+        public const short ID = 571;
+
+        public Jump()
+        {
+            Write();
+        }
+
+        public override void Write()
+        {
+            packetWriter.Write(6);
+            packetWriter.Write(ID);
+            packetWriter.Write((short)17370);
+            packetWriter.Write((short)1032);
+        }
+    }
+}
