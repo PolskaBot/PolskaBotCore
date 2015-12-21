32e2031f383f66c89acd178c14c74f0953949381
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2015-12-22 00:17:33 +0100

    resized form to fit tabcontrol size

--- PolskaBot/Bot.Designer.cs
+++ PolskaBot/Bot.Designer.cs
@@ -131,7 +131,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(378, 651);
+            this.ClientSize = new System.Drawing.Size(378, 683);
             this.Controls.Add(this.botTabs);
             this.Controls.Add(this.startButton);
             this.Controls.Add(this.stopButton);
